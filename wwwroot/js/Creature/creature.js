cb25183ea7611339895b1b3b05a82e1baee9b463
Miroslav262 <mirostavchik235@gmail.com>
2026-06-01 19:26:43 +0300

    Update creature page

---
+++ wwwroot/js/Creature/creature.js
@@ -0,0 +1,57 @@
+﻿const one_roll = function (size) {
+    return Math.floor(Math.random() * size) + 1;
+};
+
+const roll = function (count, size) {
+    let result = 0;
+    for (let i = 0; i < count; i++) {
+        result += one_roll(size);
+    }
+    return result;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelectorAll(".roll").forEach(el => {
+        el.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            const bonus = parseInt(el.dataset.bonus);
+            const r = one_roll(20);
+            const total = r + bonus;
+
+            el.classList.remove("crit", "fail");
+
+            if (r === 20) {
+                el.classList.add("crit");
+            }
+
+            if (r === 1) {
+                el.classList.add("fail");
+            }
+
+            el.innerHTML = `${bonus} → (${r} = ${total})`;
+        });
+    });
+
+
+    document.querySelectorAll(".hp_roll").forEach(el => {
+        el.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            const expr = el.dataset.hp_roll;
+
+            const [countPart, rest] = expr.split("d");
+            const [sizePart, bonusPart] = rest.split("+");
+
+            const count = parseInt(countPart);
+            const size = parseInt(sizePart);
+            const bonus = parseInt(bonusPart);
+
+            const total = roll(count, size) + bonus;
+
+            el.innerHTML = `(${expr}) → ${total}`;
+        });
+    });
+
+});
