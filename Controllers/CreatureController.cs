e54aedbe224e19b537e3dc1c742464a4809d3629
Miroslav262 <mirostavchik235@gmail.com>
2026-06-01 14:47:41 +0300

    refactor creature controller & buttons in main page

--- Controllers/CreatureController.cs
+++ Controllers/CreatureController.cs
@@ -6,38 +6,33 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace dndsitgen.Controllers
 {
-    public class CreatureLibraryController : Controller
+    public class CreatureController : Controller
     {
         private readonly CreaturesService _service;
         private int? maxId = null;
-        public CreatureLibraryController(CreaturesService service) {
+        public CreatureController(CreaturesService service) {
             _service = service;
 
         }
-        public async Task<IActionResult> Index()
-        {
 
-            int count = await _service.getCount();
-            return View("~/Views/Home/Index.cshtml", count);
-        }
         [HttpGet("/creature/random_creature")]
         public async Task<IActionResult> RandomCreature() {
             if (maxId == null) {
                 maxId = await _service.getCount();
             }
             int randomId = Random.Shared.Next(1, maxId.Value + 1);
-            return View("~/Views/Home/Creature.cshtml", await _service.getCreature(randomId));
+            return View("~/Views/Creature/Creature.cshtml", await _service.getCreature(randomId));
         }
         [HttpPost("/creature")]
         public async Task<IActionResult> Creature(CreatureModel creatureModel)
         {
-            return View("~/Views/Home/Creature.cshtml", creatureModel);
+            return View("~/Views/Creature/Creature.cshtml", creatureModel);
         }
 
         [HttpGet("/creature/{key}")]
         public async Task<IActionResult> CreatureByKey(string key)
         {
-            return View("~/Views/Home/Creature.cshtml", await _service.getCreatureByKey(key));
+            return View("~/Views/Creature/Creature.cshtml", await _service.getCreatureByKey(key));
         }
 
     }
